2f0f85e892d34fc829293f0e80baa1a63e08092d
Stephen Clark <stephen.clark2@city-holdings.co.uk>
2022-06-20 20:56:09 +0100

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Stephen Clark on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
